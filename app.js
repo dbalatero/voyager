c1a503c9720f25930c8dc57f044fa2f775e83b0a
David Balatero <dbalatero@gmail.com>
2016-06-27 13:07:37 -0700

    no console.log please

--- app.js
+++ app.js
@@ -7,6 +7,4 @@ app.get('/', function (req, res) {
   res.sendFile('index.html', { root: __dirname })
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-});
+app.listen(3000)
